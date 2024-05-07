4831ca91b034cc2f1eccc337100ae60475d4c2f1
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-05-07 18:14:32 -0300

    Anatomia de Classes

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,25 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        
+        System.out.print("Conhecendo o método main e imprimindo");
+
+        //após declarar a variável como int, ela pode ser alterada para outro número
+        int ano = 2021;
+
+        ano = 2022;
+
+        //ao utilizar a palavra reservada final, a variável BR é definida unicamente como "Brasil", não podendo ser alterada
+        final String BR = "Brasil";
+
+        //BR = "Brazil"   (DESTA FORMA NÃO É POSSÍVEL);
+
+        /* DECLARAÇÕES DE VARIAVEIS:
+            -> DEVE CONTER APENAS LETRAS, _ (UNDERLINE), $ OU OS NÚMEROS DE 0 A 9
+            -> DEVE OBRIGATORIAMENTE SE INICIAR POR UMA LETRA (PREFERENCIALMENTE), _ OU $
+            -> DEVE INICIAR COM UMA LETRA MINUSCULA (BOA PRÁTICA)
+            -> NÃO PODE CONTER ESPAÇOS
+            -> NÃO PODEMOS USAR PALAVRAS-CHAVE
+            -> O NOME DEVE SER ÚNICO 
+        */
+    }
+}
