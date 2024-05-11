3de9818dd8fe256108a8523a61581442ff61cad8
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-05-11 20:36:52 -0300

    teoria sobre métodos

---
+++ java-tipos-e-variaveis/src/Metodos.java
@@ -0,0 +1,21 @@
+public class Metodos {
+    public static void main(String[] args) {
+        /*
+         * Uma classe é definida por atributos e métodos. Atributos são, em sua grande maioria, variáveis de diferentes tipos e valores. Os métodos, correspondem
+         * a funções ou sub-rotinas disponíveis dentro dessas nossas classes
+         * 
+         * - Deve ser nomeado como verbo
+         * - Seguir padrão camelCase (Todas as letras minúsculas com a exceção da primeira letra da segunda palavra)
+         */
+    }
+
+    //exemplos de métodos
+    
+    public double somar (int n1, int n2) {
+        return n1+n2;
+     }
+
+     public void imprimir (String texto) {
+        System.out.println("Imprimindo...");
+     }
+}
