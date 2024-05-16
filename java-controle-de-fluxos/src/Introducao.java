adf1fe0fab5b346fe725c5ad4ad62c88cc0575b9
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-05-16 17:49:33 -0300

    Introdução

---
+++ java-controle-de-fluxos/src/Introducao.java
@@ -0,0 +1,12 @@
+public class Introducao {
+    
+    /*
+     * Controle de fluxo é a habilidade de ajustar a maneira como um programa realiza suas tarefas.
+     * Por meio de instruções especiais, chamadas de comandos, essas tarefas podem ser executadas
+     * seletivamente, repetidamente ou excepcionalmente.
+     * 
+     * Estruturas condicionais: if-else, switch-case
+     * Estruturas de repetição: for, while, do-while
+     * Estruturas de exceções: try-catch-finally, throw
+     */
+}
