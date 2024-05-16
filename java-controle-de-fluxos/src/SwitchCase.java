04f3f7cf5ba9da0088651dae0ee02356fff73482
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-05-16 17:49:53 -0300

    Switch Case

---
+++ java-controle-de-fluxos/src/SwitchCase.java
@@ -0,0 +1,33 @@
+public class SwitchCase {
+
+    public static void main(String[] args) {
+        /*
+         * A estrutura switch compara o valor de cada caso com o da variável sequencialmente, e sempre que encontra
+         * um valor correspondente, executa o código associado ao caso. Para evitar que as comparações continuem a ser
+         * executadas após um caso correspondente ter sido encontrado, acrescentamos o comando break no final de cada
+         * blocode códigos. O comando break, quando executado, encerra a execução da estrutura onde ele se encontra
+         */
+
+         //programa de exemplo de medida
+
+         String sigla = "M";
+
+        switch (sigla) {
+            case "P": {
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M": {
+                System.out.println("MÉDIO");
+                break;
+            }
+            case "G": {
+                System.out.println("GRANDE");
+                break;
+            }
+            default:
+            System.out.println("INDEFINIDO");
+         } 
+    }
+    
+}
