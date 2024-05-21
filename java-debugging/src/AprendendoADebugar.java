ee264507e746b88a84bf2cffcf1532e5e2b89dc8
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-05-21 16:02:12 -0300

    Atualizando

--- java-debugging/src/AprendendoADebugar.java
+++ java-debugging/src/AprendendoADebugar.java
@@ -1,5 +1,24 @@
 public class AprendendoADebugar {
+
+    /*
+     * Debugging é um processo que tem por objetivo reduzir ou encontrar bugs no seu sistema.
+     * De uma forma geral, debugging não é uma tarefa fácil de ser executada, principalmente
+     * em um sistema complexo. 
+     * 
+     * O java em si falicita muito o processo, pois fornece maneiras de sabermos se o código 
+     * está errado, uma das coisas que facilita isso é a ferramenta de debug.
+     * 
+     */
+
     public static void main(String[] args) {
+        /*
+         * Adicionando nosso Breakpoint aqui e iniciarmos o programa por debug,
+         * podemos perceber que após imprimir a primeira linha, o código sai do
+         * método principal (main) e vai em busca do método a(). E assim o código
+         * segue até encerrar o método main. Caso algum outro método estivesse 
+         * errado, conseguiríamos perceber pelo debug.
+         */
+        
         System.out.println("Iniciando o processo de Debugging");
         a();
         System.out.println("Fim do processo de Debugging");
