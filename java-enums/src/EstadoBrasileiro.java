0153da46274c321472ca2ddbc6c75b7887252956
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-06-17 14:02:53 -0300

    atualizando

--- java-enums/src/EstadoBrasileiro.java
+++ java-enums/src/EstadoBrasileiro.java
@@ -1,4 +1,13 @@
 public enum EstadoBrasileiro {
+
+    /*
+     * ENUM é um tipo especial de classe onde os objetos são previamente criados, imutáveis e disponípveis por toda a aplicação
+     * 
+     * Usamos ENUM quando o nosso modelo de negócio contém objetos de mesmo contexto que já existem de pré´-estabelecida com
+     * a certeza de não haver tanta alteração de valores.
+     * 
+     */
+
     SAO_PAULO ("SP", "São Paulo"),
     RIO_DE_JANEIRO ("RJ", "Rio de Janeiro"),
     PIAUI ("PI", "Piauí"),
